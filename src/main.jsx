5ea364aeeba04c88f338acb4328def0654a6c4da
Mohitsen11 <mohitsen0102@gmail.com>
2023-10-31 16:48:49 +0530

    Added Toaster library and updated the code

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
+
+import App from './App.jsx'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+    <Toaster/>
+  </BrowserRouter>
 )
