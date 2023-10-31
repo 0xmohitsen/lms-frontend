962d9775d77aa76615a067a5790baf7be358cfa1
Mohitsen11 <mohitsen0102@gmail.com>
2023-10-31 17:17:35 +0530

    Added semicolon at the end of the instruction

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-import './index.css'
+import './index.css';
 
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import { Toaster } from 'react-hot-toast'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
 
 import App from './App.jsx'
 
